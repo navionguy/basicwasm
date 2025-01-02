7a4967d37c8ddc49c098b03be2f19d4bb7bab353
Don Hinds <hindsdw@gmail.com>
2025-01-02 11:05:05 -0500

    cleaning up some code

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -1319,7 +1319,8 @@ func Test_InkeyExpression(t *testing.T) {
 	tests := []struct {
 		inp string
 	}{
-		{inp: `X = ABS(-5) : END`},
+		//{inp: `X = ABS(-5) : END`},
+		//{inp: `X$ = HEX$(35) : END`},
 		{inp: `X$ = INKEY$ : END`},
 	}
 
